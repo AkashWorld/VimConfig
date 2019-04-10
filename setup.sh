c36c9e416a18418f2758f8d2ce55995a3a2ccd9c
Khalid Akash <khalkash@gmail.com>
2019-04-10 14:09:56 -0400

    Updated relative pathing in setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 set -x
-ln -rs ./vimrc ../.vimrc
-sudo git submodule update --init
+ln -rs ~/.vim/vimrc ~/.vimrc
+cd ~/.vim
+sudo git submodule update --init --recursive
 sudo apt-get install python3
 sudo apt-get install python3-dev
 python3 ./bundle/YouCompleteMe/install.py --clang-completer

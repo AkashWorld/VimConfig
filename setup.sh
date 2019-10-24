e7138559dd605e6cbcf6826d65085f358c949873
Khalid Akash <khalkash@gmail.com>
2019-10-24 00:25:00 -0400

    Added cmake to install

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ set -x
 ln -rs ~/.vim/vimrc ~/.vimrc
 cd ~/.vim
 sudo git submodule update --init --recursive
+sudo apt-get install cmake
 sudo apt-get install python3
 sudo apt-get install python3-dev
 python3 ./bundle/YouCompleteMe/install.py --clang-completer
